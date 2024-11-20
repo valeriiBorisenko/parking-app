d26bcdb5d9bc4c663f520720d8132721f13c5de3
Valerii Borisenko <valerii.borisenko@gmail.com>
2024-11-20 16:15:38 +0100

    update CustomerDaoCollection

--- src/main/java/se/lexicon/data/dao/CustomerDaoCollection.java
+++ src/main/java/se/lexicon/data/dao/CustomerDaoCollection.java
@@ -1,4 +1,50 @@
 package se.lexicon.data.dao;
 
-public class CustomerDaoCollection {
+import se.lexicon.model.Customer;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomerDaoCollection implements CustomerDao {
+
+    private Collection<Customer> customers;
+
+    public CustomerDaoCollection(Collection<Customer> customers) {
+        this.customers = customers;
+    }
+
+    @Override
+    public Customer create(Customer customer) {
+        return this.customers.add(customer) ? customer : null;
+    }
+
+    @Override
+    public Customer findById(int id) {
+        for (Customer customer: this.customers) {
+            if (customer.getId() == id) {
+                return customer;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+        this.customers.remove(findById(id));
+    }
+
+    @Override
+    public Customer findByReservationId(int id) {
+        for (Customer customer: this.customers) {
+            if (customer.getReservation() != null && customer.getReservation().getId() == id) {
+                return customer;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Collection<Customer> findAll() {
+        return Collections.unmodifiableCollection(this.customers);
+    }
 }
